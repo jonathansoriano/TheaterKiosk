ea60dff4ad154580373ef440dba95436f8b01d40
jonathansoriano <158002831+jonathansoriano@users.noreply.github.com>
2024-02-07 21:44:33 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int wristBand =21;
+        int age= 0;
+
+        System.out.print("Enter your age: ");
+        age = in.nextInt();
+        if (age >= wristBand)
+        {
+            System.out.println("You get a wristband!");
+        }
+    }
+}
